82a933ceb16775c6f2837b67d86aac2025da9f32
moytri <moytritopa@gmail.com>
2015-11-24 17:38:16 +0000

    disconnect through socket.io

--- index.js
+++ index.js
@@ -27,10 +27,22 @@ var express=require('express'),
                socket.nickname=message;
                nicknames.push(socket.nickname);
                io.sockets.emit('usernames',nicknames);
-               //updateNicknames();
+               updateNicknames();
            }
         });
         
+       function updateNicknames(){
+            io.sockets.emit('usernames', nicknames);
+        }
 
-        
+        socket.on('send message', function(message){
+            io.sockets.emit('new message', {msg: message, nick: socket.nickname});
+        });
+
+        socket.on('disconnect', function(message){
+            if(!socket.nickname) return;
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            updateNicknames();
+        });
+
     });
