e86180e1ddb415e00aa8f6dc144e461d1eb4eb2e
moytri <moytritopa@gmail.com>
2015-11-24 12:59:54 +0000

    First commit for the Logic of user name

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ var express=require('express'),
         res.sendfile(__dirname+'/index.html');
     });
     
+    io.sockets.on('connection', function(socket){
+        socket.on('new user',function(message,callback)
+        {
+           if(nicknames.indexOf(message)!=-1) {
+               callback(false);
+           }else {
+               callback(true);
+               socket.nickname=message;
+               nicknames.push(socket.nickname);
+               io.sockets.emit('usernames',nicknames);
+               //updateNicknames();
+           }
+        });
+        
+        function updateNicknames(){
+            io.sockets.emit('usernames', nicknames);
+        }
+
+        socket.on('send message', function(message){
+            io.sockets.emit('new message', {msg: message, nick: socket.nickname});
+        });
+
+        socket.on('disconnect', function(message){
+            if(!socket.nickname) return;
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            updateNicknames();
+        }); 
+        
+    });
