d42453cf4f1ac7227f6b2a1809aa76e1c4872513
moytri <moytritopa@gmail.com>
2015-11-25 13:59:18 +0000

    Modification

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var express=require('express'),
-    app=express(),
-    server=require('http').createServer(app),
-    io=require('socket.io').listen(server);
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+	
     server.listen(process.env.PORT, process.env.IP);
     
    
