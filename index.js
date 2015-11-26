57469742f69b21ed941f8d46ba39820501ceb6b4
moytri <moytritopa@gmail.com>
2015-11-26 07:04:26 +0000

    Another way to connect server is included in index.js, It can read the index.html.Bu can not pass username to server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 /*jshint curly:true, debug:true */
 var express = require('express'),
-	app = express(),
+	app = express(),fs = require('fs'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	
-    server.listen(process.env.PORT, process.env.IP);
+    server.listen(Number(process.env.PORT||8080));
     
-   
     // server.listen(8000);
     
     //requesting localhost to connect to index.html
@@ -14,6 +12,47 @@ var express = require('express'),
         res.sendFile(__dirname+'/index.html');
     });
     
+    /*Another way for server connection 
+    
+    var fs = require('fs');
+    var express = require('express');
+    var app = express();
+	var server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+    
+    app.get('*', function (req, res) {
+
+    var urlReading = req.url;
+    if (urlReading == "/")
+    {
+        urlReading = "/index.html";
+    }
+    urlReading = __dirname+'/index.html'+ urlReading;
+
+    console.log("Loading: " + urlReading);
+
+    fs.readFile(urlReading, function (err, html) {
+        if (err) {
+            console.log("Could not find " + urlReading)
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("<html><head><title>Page could not be found</title></head><body><h1>Page could not be found</h1></body></html>");
+        }
+        else
+        {
+            console.log("Found " + urlReading)
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html);
+        }
+
+    });
+});
+
+
+    app.listen(process.env.PORT, process.env.IP);
+    
+    */
+    
+   
     
     // usernames which are currently connected to the chat
     var nicknames=[];
